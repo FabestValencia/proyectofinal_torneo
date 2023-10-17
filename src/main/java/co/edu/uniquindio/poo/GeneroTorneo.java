05ceb8369d463f2c85baf1be781f5c28a9038cd3
FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
2023-10-17 01:10:58 -0500

    Se modificaron los enums TipoTorneo y GeneroTorneo

--- src/main/java/co/edu/uniquindio/poo/GeneroTorneo.java
+++ src/main/java/co/edu/uniquindio/poo/GeneroTorneo.java
@@ -1,5 +1,5 @@
 package co.edu.uniquindio.poo;
 
 public enum GeneroTorneo {
-    
+    MASCULINO, FEMENINO, MIXTO;
 }
--- src/main/java/co/edu/uniquindio/poo/TipoTorneo.java
+++ src/main/java/co/edu/uniquindio/poo/TipoTorneo.java
@@ -1,5 +1,5 @@
 package co.edu.uniquindio.poo;
 
 public enum TipoTorneo {
-    
+    LOCAL, REGIONAL, NACIONAL, MUNDIAL;
 }
