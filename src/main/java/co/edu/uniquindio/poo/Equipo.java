bf4abac3e343223e60891fac6bef4f6305b0336d
FabianValencia23 <127154183+FabianValencia23@users.noreply.github.com>
2023-10-24 23:26:06 -0500

    Se modifico la clase equipo

--- src/main/java/co/edu/uniquindio/poo/Equipo.java
+++ src/main/java/co/edu/uniquindio/poo/Equipo.java
@@ -1,7 +1,29 @@
 package co.edu.uniquindio.poo;
 
 public class Equipo {
+    
     private String nombreEquipo;
     private Persona representante;
     
+    public Equipo(String nombreEquipo, Persona representante) {
+        this.nombreEquipo = nombreEquipo;
+        this.representante = representante;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public Persona getRepresentante() {
+        return representante;
+    }
+
+    public void setRepresentante(Persona representante) {
+        this.representante = representante;
+    }
+        
 }
